79cd598568506d179b3c8b40712cdd7544b5b1e8
Seba Gines <sebastian.gines@gmail.com>
2022-08-28 15:34:23 -0300

    Agregado de ejemplos de excepciones de formato, overflox y divided by cero

--- Excepciones/Excepciones/ejemplo1/Form1.Designer.cs
+++ Excepciones/Excepciones/ejemplo1/Form1.Designer.cs
@@ -32,28 +32,31 @@
             this.txtUno = new System.Windows.Forms.TextBox();
             this.txtDos = new System.Windows.Forms.TextBox();
             this.lblResultado = new System.Windows.Forms.Label();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(54, 124);
+            this.btnCalcular.Location = new System.Drawing.Point(40, 125);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.Size = new System.Drawing.Size(44, 23);
             this.btnCalcular.TabIndex = 0;
-            this.btnCalcular.Text = "Calcular Suma";
+            this.btnCalcular.Text = "+";
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // txtUno
             // 
-            this.txtUno.Location = new System.Drawing.Point(54, 40);
+            this.txtUno.Location = new System.Drawing.Point(85, 34);
             this.txtUno.Name = "txtUno";
             this.txtUno.Size = new System.Drawing.Size(100, 20);
             this.txtUno.TabIndex = 1;
             // 
             // txtDos
             // 
-            this.txtDos.Location = new System.Drawing.Point(54, 81);
+            this.txtDos.Location = new System.Drawing.Point(85, 75);
             this.txtDos.Name = "txtDos";
             this.txtDos.Size = new System.Drawing.Size(100, 20);
             this.txtDos.TabIndex = 2;
@@ -63,23 +66,57 @@
             this.lblResultado.AutoSize = true;
             this.lblResultado.Location = new System.Drawing.Point(54, 169);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
             this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "=";
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(141, 125);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(44, 23);
+            this.btnDividir.TabIndex = 4;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(90, 125);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(44, 23);
+            this.btnRestar.TabIndex = 5;
+            this.btnRestar.Text = "-";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(191, 125);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(44, 23);
+            this.btnMultiplicar.TabIndex = 6;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
             // 
             // frmCalculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(210, 235);
+            this.ClientSize = new System.Drawing.Size(267, 196);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnDividir);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.txtDos);
             this.Controls.Add(this.txtUno);
             this.Controls.Add(this.btnCalcular);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(210, 235);
+            this.MaximumSize = new System.Drawing.Size(283, 235);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(210, 235);
+            this.MinimumSize = new System.Drawing.Size(283, 235);
             this.Name = "frmCalculadora";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculadora";
@@ -94,6 +131,9 @@
         private System.Windows.Forms.TextBox txtUno;
         private System.Windows.Forms.TextBox txtDos;
         private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
     }
 }
 
--- Excepciones/Excepciones/ejemplo1/Form1.cs
+++ Excepciones/Excepciones/ejemplo1/Form1.cs
@@ -20,10 +20,73 @@ namespace ejemplo1
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             int a, b, r;
-            a = int.Parse(txtUno.Text);
-            b = int.Parse(txtDos.Text);
-            r = a + b;
-            lblResultado.Text = "= " + r;
+            try
+            {
+                a = int.Parse(txtUno.Text);
+                b = int.Parse(txtDos.Text);
+                r = a + b;
+                lblResultado.Text = "= " + r;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Por favor cargar solo números..");
+            }           
+        }
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtUno.Text);
+                b = int.Parse(txtDos.Text);
+                r = a - b;
+                lblResultado.Text = "= " + r;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Por favor cargar solo números..");
+            }
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtUno.Text);
+                b = int.Parse(txtDos.Text);
+                r = a / b;
+                lblResultado.Text = "= " + r;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Por favor cargar solo números..");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("No se puede dividir por cero (0)...");
+            }
+            catch (OverflowException ex){
+                MessageBox.Show("El resultado es un número muy grande...");
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtUno.Text);
+                b = int.Parse(txtDos.Text);
+                r = a * b;
+                lblResultado.Text = "= " + r;
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Por favor cargar solo números..");
+            }
+
         }
     }
 }
